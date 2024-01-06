a9d814a42ecb04dbfedf4da06c4a18035299d27a
k-mushket <k.mushcat@gmail.com>
2024-01-06 22:26:47 +0200

    based structure

--- lib/pages/notepad_page.dart
+++ lib/pages/notepad_page.dart
@@ -8,18 +8,7 @@ class Notepad extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: GridView.builder(
-          padding: EdgeInsets.all(14),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 8,
-          ),
-          itemCount: 50,
-          itemBuilder: (context, index) => Container(
-            color: Colors.amber,
-          ),
-        ),
+        body: Column(),
       ),
     );
   }
